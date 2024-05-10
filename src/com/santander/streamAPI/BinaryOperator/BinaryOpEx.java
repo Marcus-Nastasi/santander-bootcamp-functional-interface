f5029108e65536830f5738f0c3127650b73606cf
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 13:40:12 -0300

    testing other forms of reducing;

--- src/com/santander/streamAPI/BinaryOperator/BinaryOpEx.java
+++ src/com/santander/streamAPI/BinaryOperator/BinaryOpEx.java
@@ -9,8 +9,10 @@ public class BinaryOpEx {
     public static void main(String[] args) {
         List<Double> doubleList = Arrays.asList(1.99, 2.35, 3.40, 4.0, 5.75);
 
-        BinaryOperator<Double> sumList = (n1, n2) -> n1 + n2;
+        //BinaryOperator<Double> sumList = (n1, n2) -> n1 + n2;
 
-        doubleList.stream().reduce(sumList).stream().forEach(System.out::println);
+        doubleList.stream().reduce((n1, n2) -> n1 + n2).stream().forEach(System.out::println);
     }
 }
+
+
