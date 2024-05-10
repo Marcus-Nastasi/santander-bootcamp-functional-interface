91ff70d914d5c977d731e0f24aca5d81667a938d
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 13:26:57 -0300

    testing filter methods on Predicate;

--- src/com/santander/streamAPI/Predicate/PredicateEx.java
+++ src/com/santander/streamAPI/Predicate/PredicateEx.java
@@ -9,9 +9,9 @@ public class PredicateEx {
     public static void main(String[] args) {
         List<String> langs = Arrays.asList("c", "java", "typescript", "rust", "go");
 
-        Predicate<String> stringPredicate = w -> w.length() >= 5;
+        //Predicate<String> stringPredicate = w -> w.length() >= 5;
 
-        langs.stream().filter(stringPredicate).forEach(System.out::println);
+        langs.stream().filter(w -> w.length() >= 5).forEach(System.out::println);
     }
 }
 
