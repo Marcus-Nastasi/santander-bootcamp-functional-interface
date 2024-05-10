84a7842c803edfaa27baffba93b0f0c18bdcf5ee
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 13:39:32 -0300

    implemented methods;

--- src/com/santander/streamAPI/BinaryOperator/BinaryOpEx.java
+++ src/com/santander/streamAPI/BinaryOperator/BinaryOpEx.java
@@ -1,4 +1,16 @@
 package com.santander.streamAPI.BinaryOperator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class BinaryOpEx {
+
+    public static void main(String[] args) {
+        List<Double> doubleList = Arrays.asList(1.99, 2.35, 3.40, 4.0, 5.75);
+
+        BinaryOperator<Double> sumList = (n1, n2) -> n1 + n2;
+
+        doubleList.stream().reduce(sumList).stream().forEach(System.out::println);
+    }
 }
