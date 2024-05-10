4cf4e04873672bb781c54b3ca730972284e87f0b
rolemberg <vinnie.nsts@gmail.com>
2024-05-09 23:49:09 -0300

    created SuplierEx package and class;

---
+++ src/com/santander/streamAPI/Suplier/SuplierEx.java
@@ -0,0 +1,14 @@
+package com.santander.streamAPI.Suplier;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SuplierEx {
+
+    public static void main(String[] args) {
+        Supplier<String> wellcome = () -> "Hello, wellcome to the Suplier test!";
+
+
+    }
+}
+
