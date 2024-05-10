8be58c9e7326e94fb041430f90cee62db8a83bc2
rolemberg <vinnie.nsts@gmail.com>
2024-05-09 23:51:18 -0300

    config class SuplierEx;

--- src/com/santander/streamAPI/Suplier/SuplierEx.java
+++ src/com/santander/streamAPI/Suplier/SuplierEx.java
@@ -2,13 +2,17 @@ package com.santander.streamAPI.Suplier;
 
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SuplierEx {
 
     public static void main(String[] args) {
         Supplier<String> wellcome = () -> "Hello, wellcome to the Suplier test!";
 
+        List<String> wellcomeList = Stream.generate(wellcome).limit(5).collect(Collectors.toList());
 
+        wellcomeList.forEach(w -> System.out.println(w));
     }
 }
 
