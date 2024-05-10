738989ae34d62e7a5981016308cefdb61091c7f6
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 13:24:06 -0300

    creating PredicateEx methods and testing;

--- src/com/santander/streamAPI/Predicate/PredicateEx.java
+++ src/com/santander/streamAPI/Predicate/PredicateEx.java
@@ -1,4 +1,18 @@
 package com.santander.streamAPI.Predicate;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class PredicateEx {
+
+    public static void main(String[] args) {
+        List<String> langs = Arrays.asList("c", "java", "typescript", "rust", "go");
+
+        Predicate<String> stringPredicate = w -> w.length() >= 5;
+
+        langs.stream().filter(stringPredicate).forEach(System.out::println);
+    }
 }
+
+
